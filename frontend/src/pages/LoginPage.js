f030a36d9e042a9358c642ebd8acda31aef677e5
bkwag <kwag93@naver.com>
2021-08-06 20:13:25 +0900

    feat: 42oauth 액세스 토큰 받음

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import Header from '../common/Header';
 import queryString from 'query-string';
-
-const Redirect =
-  'https://api.intra.42.fr/oauth/authorize?client_id=c99cdf4885e7223c1e66e3060d56b9aac2dd5927b765593c669c78613a5b679d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code';
-
+import axios from 'axios';
 const LoginRequestEvent = ({ location }) => {
   const query = queryString.parse(location.search);
-  const error = query.error == 'access_denied';
-  const usercode = queryString.parse(location.serch);
-  console.log(query);
+  const code = query.code;
+  //   const error = query.error == 'access_denied';
+  //   const usercode = queryString.parse(location.serch);
+
+  const data = axios({
+    method: 'post',
+    url: 'https://api.intra.42.fr/oauth/token',
+    data: {
+      grant_type: 'authorization_code',
+      client_id:
+        'c99cdf4885e7223c1e66e3060d56b9aac2dd5927b765593c669c78613a5b679d',
+      client_secret:
+        'd49df9404304e2bff0365eaec854556af2ce711706f7498caed2d1d5cdd882b3',
+      code: code,
+      redirect_uri: 'http://localhost:3000/login',
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => console.log(error));
   return (
     <React.Fragment>
       <Header />
-      <a href={Redirect}>login</a>
+      <a
+        href="https://api.intra.42.fr/oauth/authorize?client_id=c99cdf4885e7223c1e66e3060d56b9aac2dd5927b765593c669c78613a5b679d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code
+"
+      >
+        login
+      </a>
     </React.Fragment>
   );
 };
