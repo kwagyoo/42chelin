530327696d7f94773c2e5d712c42ba98b3cd587b
bkwag <kwag93@naver.com>
2021-08-09 12:07:13 +0900

    feat: login v2/me 를 통해 username까지 받아옴

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,13 +2,29 @@ import React from 'react';
 import Header from '../common/Header';
 import queryString from 'query-string';
 import axios from 'axios';
-const LoginRequestEvent = ({ location }) => {
+
+const GetUsername = async (token) => {
+  console.log('Bearer ' + token);
+  await axios({
+    method: 'GET',
+    url: 'https://api.intra.42.fr/v2/me',
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => console.log(error));
+};
+
+const LoginRequest = async ({ location }) => {
   const query = queryString.parse(location.search);
   const code = query.code;
   //   const error = query.error == 'access_denied';
   //   const usercode = queryString.parse(location.serch);
 
-  const data = axios({
+  await axios({
     method: 'post',
     url: 'https://api.intra.42.fr/oauth/token',
     data: {
@@ -22,18 +38,22 @@ const LoginRequestEvent = ({ location }) => {
     },
   })
     .then((res) => {
-      console.log(res);
+      const token = res.data.access_token;
+      GetUsername(token);
     })
     .catch((error) => console.log(error));
+};
+
+const LoginRequestEvent = ({ location }) => {
+  LoginRequest({ location });
+  const URL =
+    'https://api.intra.42.fr/oauth/authorize?client_id=c99cdf4885e7223c1e66e3060d56b9aac2dd5927b765593c669c78613a5b679d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code';
+  //   const error = query.error == 'access_denied';
+  //   const usercode = queryString.parse(location.serch);
   return (
     <React.Fragment>
       <Header />
-      <a
-        href="https://api.intra.42.fr/oauth/authorize?client_id=c99cdf4885e7223c1e66e3060d56b9aac2dd5927b765593c669c78613a5b679d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code
-"
-      >
-        login
-      </a>
+      <a href={URL}>login</a>
     </React.Fragment>
   );
 };
