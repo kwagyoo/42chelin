3f74d67b9ed0b83460365bc64da0350216c7bd01
bkwag <kwag93@naver.com>
2021-10-14 11:15:39 +0900

    env fix

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -1,11 +1,13 @@
 import client from './client';
 
-export const getToken = (code) =>
-  client.post(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-token/`, {
+export const getToken = (code) => {
+  client.post(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-token`, {
     code: code,
   });
+};
 
-export const getUser = (token) =>
-  client.get(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-user/`, {
+export const getUser = (token) => {
+  client.get(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-user`, {
     token: token,
   });
+};
