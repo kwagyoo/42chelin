ca60f25cfcaab56fe09f7af70f6eef70521ca449
bkwag <kwag93@naver.com>
2021-10-14 14:50:24 +0900

    env fix

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -1,17 +1,11 @@
 import client from './client';
 
 export const getToken = (code) =>
-  client.post(
-    `https://d2d5oodqrc.execute-api.ap-northeast-2.amazonaws.com/Stage/get-token`,
-    {
-      code: code,
-    },
-  );
+  client.post(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-token`, {
+    code: code,
+  });
 
 export const getUser = (token) =>
-  client.get(
-    `https://d2d5oodqrc.execute-api.ap-northeast-2.amazonaws.com/Stage/get-user`,
-    {
-      token: token,
-    },
-  );
+  client.get(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-user`, {
+    token: token,
+  });
