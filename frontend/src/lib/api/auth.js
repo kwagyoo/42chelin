b430e6f7e78c58002e6508d50d859fd6625f20a3
유현우 <hyunyoo@yuhyeon-uui-MacBookPro.local>
2021-10-14 09:27:44 +0900

    fix : url 미적용 부분 수정

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -1,17 +1,11 @@
 import client from './client';
 
 export const getToken = (code) =>
-  client.post(
-    `${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/requestlogintoken/`,
-    {
-      code: code,
-    },
-  );
+  client.post(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-token/`, {
+    code: code,
+  });
 
 export const getUser = (token) =>
-  client.get(
-    `${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/checkTokenFunction`,
-    {
-      token: token,
-    },
-  );
+  client.get(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-user/`, {
+    token: token,
+  });
