155ebb073339459c47a78b1a202b7ed49f7612e9
유현우 <hyunyoo@yuhyeon-uui-MacBookPro.local>
2021-10-14 18:59:55 +0900

    feat : change save store data function

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -9,3 +9,12 @@ export const getUser = (token) =>
   client.get(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/get-user`, {
     token: token,
   });
+
+export const saveStoreData = (request) => {
+  const userToken = localStorage.getItem('token');
+  if (!userToken) return null;
+  console.log(request);
+  client.post(`${process.env.REACT_APP_BACKEND_ENDPOINT_URL}/save-store-data`, {
+    token: userToken,
+  });
+};
