58e95c95658a86b8a93115b2277699f13cb629b4
Ryuna4347 <yhw7979@naver.com>
2022-01-04 14:11:17 +0900

    feat:비밀번호 초기화/변경 추가

--- frontend/src/pages/PwResetPage.js
+++ frontend/src/pages/PwResetPage.js
@@ -1,7 +1,8 @@
 import QueryString from 'qs';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { fetchResetPassword } from '../lib/api/auth';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,16 +10,34 @@ const Wrapper = styled.div`
 
 const PwResetPage = ({ location }) => {
   const history = useHistory();
+
+  const requestPassword = useCallback(
+    async (code) => {
+      try {
+        await fetchResetPassword(code);
+        console.log('d');
+        history.push('/login');
+      } catch (err) {
+        alert('오류가 발생하여 이전 페이지로 이동합니다.');
+        history.goBack();
+      }
+    },
+    [history],
+  );
+
   useEffect(() => {
     const query = QueryString.parse(location.search, {
       ignoreQueryPrefix: true,
     });
     if (!query.code) history.goBack();
-  }, [location.search, history]);
+    else {
+      requestPassword(query.code);
+    }
+  }, [location.search, history, requestPassword]);
 
   return (
     <Wrapper>
-      <div> 패스워드 리셋</div>
+      <div> 패스워드 리셋 진행중...</div>
     </Wrapper>
   );
 };
